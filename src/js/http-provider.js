ccbd8dc63a9d9f13ff5ea44bbc5c7a7e4d375abc
Fabián Machuca <machucagelvez@gmail.com>
2022-10-05 09:19:42 -0500

    Consumir API

--- src/js/http-provider.js
+++ src/js/http-provider.js
@@ -1,4 +1,5 @@
 const jokeUrl = 'https://api.chucknorris.io/jokes/random'
+const urlUsuarios = 'https://reqres.in/api/users?page=2'
 
 const obtenerChiste = async () => {
   try {
@@ -12,4 +13,10 @@ const obtenerChiste = async () => {
   }
 }
 
-export { obtenerChiste }
+const obtenerUsuarios = async () => {
+  const resp = await fetch(urlUsuarios)
+  const { data: usuarios } = await resp.json()
+  return usuarios
+}
+
+export { obtenerChiste, obtenerUsuarios }
